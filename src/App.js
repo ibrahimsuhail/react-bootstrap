98117a1c63a5766d9705fac2d9a6b350a6261ed2
ibrahimsuhail <ferrari2645second@gmail.com>
2021-07-06 04:31:20 -0400

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import React from 'react';
 import './style.css';
-import { Button, Alert, ListGroup, Container, Row, Col } from 'react-bootstrap';
+import {
+  Button,
+  Alert,
+  ListGroup,
+  Container,
+  Row,
+  Col,
+  Navbar,
+  Nav
+} from 'react-bootstrap';
 import Image from 'react-bootstrap/Image';
 import React, { useState, useEffect } from 'react';
 
 const App = () => (
-  <Container fluid>
-    <Row>
-      <Col>1 of 3</Col>
-      <Col xs={6}>2 of 3 (wider)</Col>
-      <Col>3 of 3</Col>
-    </Row>
-  </Container>
+  <>
+    <Navbar>
+      <Container>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+
+    <Container fluid>
+      <Row>
+        <Col sm className="lcol">
+          1 of 3
+        </Col>
+        <Col md>
+          <pg />
+        </Col>
+        <Col sm>3 of 3</Col>
+      </Row>
+    </Container>
+  </>
 );
+const pg = () => {
+  <Alert>
+    <p>
+      2022/2023 Application for Correction of Residential Property Tax
+      Assessment (Deadline April 30, 2021)
+    </p>
+  </Alert>;
+};
 export default App;
